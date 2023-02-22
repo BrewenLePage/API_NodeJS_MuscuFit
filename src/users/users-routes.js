1ba0108b836c7fd0d29f9b1bab157f7261ef4d73
Le Page Brewen <brewenlepage@gmail.com>
2023-02-22 16:17:25 +0100

    update git ignore

---
+++ src/users/users-routes.js
@@ -0,0 +1,13 @@
+module.exports = app => {
+    const users = require('/users-controller');
+    const router = require('express').Router();
+
+    router.get("/users-list", users);
+
+    router.put("/users-update",users)
+
+    router.delete("/users-delete",users)
+
+    // -- To declare the prefix path of your API service
+    app.use("/Muscufit/api/v1", router)
+}
